177ceeef8c2092d78d93502448d7e74e4f11843a
Victoria Wu <victoria.chen.wu@gmail.com>
2013-02-26 16:32:06 -0600

    send now working - changed to strlen

--- wu_victoria_proj1.cpp
+++ wu_victoria_proj1.cpp
@@ -1,6 +1,15 @@
+#ifndef DEBUG
+#define DEBUG 1
+#endif
+
+
 #include <process.h>	//multi threading
-#include <winsock.h>
+#include <winsock.h>	
 #include <iostream>	//cerr
+#include <sstream>	
+#include <string>
+#include <cstdlib>	//rand, srand
+#include <time.h>	//srand time
 
 #pragma comment(lib, "wsock32.lib")	//link winsock lib
 #pragma comment(lib, "libcmt.lib")	//for process.h
@@ -9,6 +18,13 @@
 SOCKET connectsock(const char*, const char*);
 void UDP_IM_Client(const char* , const char* );
 void listener(void* socketNum);		//monitor for messages from server
+
+/*
+ * IM Client Functions
+ */
+void signIn(int& msgNum, std::string name, SOCKET s);
+void logOut(int& msgNum, std::string name, SOCKET s);
+
 
 int main(int argc, char **argv)	{
 	
@@ -20,6 +36,8 @@ int main(int argc, char **argv)	{
 	if(argc == 3)	{
 		serverName = argv[1];
 		portNum = argv[2];
+		std::cout << "Server IP: " << serverName << std::endl;
+		std::cout << "Port Num : " << portNum << std::endl;
 	}
 	else	{
 		std::cerr << "Need server name and port number.";
@@ -35,9 +53,9 @@ int main(int argc, char **argv)	{
 
 	UDP_IM_Client(serverName, portNum);
 
-	while(true)	{
-	}
+	
 
+	std::cout << "Goodbye!" << std::endl;
 	WSACleanup();
 	return 0;
 
@@ -47,19 +65,109 @@ int main(int argc, char **argv)	{
 
 void UDP_IM_Client(const char* serverName, const char* portNum)	{
 
-	std::cout << "Yohoo!"<< std::endl;
 
+	//Generate random num for message sequencing.
+	srand(time(NULL));
+	//int msgNum = rand() % 1000 + 10000;
+	int msgNum = 12345;
+	if(DEBUG)	
+		std::cout << "Generating msg num " << msgNum <<std::endl;
+	
+	//Connect to server.	
 	SOCKET s;
 	s = connectsock(serverName, portNum);
+	if(DEBUG)
+		std::cout << "Connected to socket " << s << std::endl;
 
 	//Start thread to monitor connection to server.
 	_beginthread(listener, 0, (void*) &s);	
 	
 	//Sign on to server.
+	std::string name;
+	std::cout << "Enter your IM name: ";
+	std::cin >> name;
+		signIn(msgNum,name, s);signIn(msgNum,name, s);
+
+	//Do stuff
+	//
 	
-		
+	std::string input;
+	while(true)	{
+		std::cin >> input;
+		if(input == "q")	
+			break;
+	}		
+	
+	//Sign off.
+	logOut(msgNum, name, s);	
+
 	//on quit, exit
 	closesocket(s);
+}
+
+/*
+ * signIn
+ * signs into server.
+ * @param name user's desired handle
+ * @param msgNum sequential msg num
+ * @param s socket num
+ */
+void signIn(int& msgNum, std::string name, SOCKET s)	{
+	std::cout << "Signing in as " << name << std::endl;
+	
+	char num[10];
+	itoa(msgNum, num, 10);
+
+	std::cout << "MSG NUM : " << num << std::endl;
+
+	char buf[256];
+       	buf[256] = '\0';
+	std::stringstream ss;
+
+	ss << num << ";1;" << name;
+
+	std::string message = ss.str();
+	
+	
+
+	int bytes_sent = send(s, message.c_str(), strlen(message.c_str() ), 0);
+	if(bytes_sent == SOCKET_ERROR)	{
+		std::cerr << "error in sending sign in msg" << std::endl;	
+	}
+
+	if(DEBUG)	{
+		std::cout << "Sent " << bytes_sent << "bytes. " << std::endl;	
+		std::cout << "Message: " << message.c_str() << std::endl;
+	}
+	msgNum++;
+}
+
+/*
+ * logOut
+ * log out of server.
+ * @param s socket num
+ * @param msgNum sequential msg number
+ */
+
+void logOut(int& msgNum, std::string name, SOCKET s)	{
+	std::cout << "Logging off... " << std::endl;
+	
+	char num[5];
+	itoa(msgNum, num, 10);
+	std::stringstream ss;
+
+	ss << num << ";3;" << name;
+
+	std::string message = ss.str();
+
+	int bytes_sent = send(s, message.c_str(), sizeof(message.c_str()), 0);
+	if(bytes_sent == SOCKET_ERROR)	{
+		std::cerr << "Error in sending log off msg" << std::endl;
+	}
+	if(DEBUG)	{
+		std::cout << "Sent: " << bytes_sent << "bytes." <<std::endl;
+		std::cout << "Msg: " << message << std::endl;
+	}
 }
 
 /*
@@ -69,7 +177,23 @@ void UDP_IM_Client(const char* serverName, const char* portNum)	{
  * @param socketNum
  */
 void listener(void* socketNum)	{
-	std::cout << "Listening for server messages..." <<std::endl;
+	if(DEBUG)
+		std::cout << "Listening for server messages..." <<std::endl;
+	
+	SOCKET s = *( (SOCKET*)socketNum);
+	if(DEBUG)
+		std::cout << "Listening to socket " << s << std::endl;
+	const int bufferLength = 200;
+	char recvbuf[bufferLength];
+	
+	while(true)	{
+		if( recv(s, recvbuf, bufferLength, 0) == SOCKET_ERROR)	{
+			std::cerr << "Error in recv " << std::endl;
+		}
+		else	{
+			printf( "FROM THE VOID you see a msg-> %s", recvbuf); 
+		}
+	}
 }
 
 /*
@@ -89,7 +213,7 @@ SOCKET connectsock(const char* serverName, const char* portNum)	{
 	memset(&sin, 0, sizeof(sin));
 	sin.sin_family = AF_INET;
 	sin.sin_port = htons((u_short)atoi(portNum)); 
-	sin.sin_addr.s_addr = inet_addr(serverName);
+	sin.sin_addr.s_addr = (inet_addr(serverName));
 	
 	//map protocol name to protocol number, define type
 	ppe = getprotobyname(transport);
