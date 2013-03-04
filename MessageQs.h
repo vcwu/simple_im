c8a773c29cf8df3606778136853cbb473f3ffda8
Victoria Wu <victoria.chen.wu@gmail.com>
2013-03-03 19:55:14 -0600

    putters done

--- MessageQs.h
+++ MessageQs.h
@@ -10,14 +10,46 @@
 
 class MessageQs	{
 	private:
+		CRITICAL_SECTION critSec;	
 		std::queue<std::string> notifications;
 		std::queue<std::string> messages;
 		std::queue<std::string> acks;
+		std::queue<std::string> fileChunks;
 	public:
 		MessageQs();
 		~MessageQs();
-
+		
+		void putNotification(std::string m);
+		void putMessage(std::string m);
+		void putAck(std::string m);
+		void putFileChunk(std::string m);
 };
 
-MessageQs::MessageQs()	{}
+MessageQs::MessageQs()	{
+	InitializeCriticalSection(&critSec);
+}
 MessageQs::~MessageQs()	{}
+
+void MessageQs::putNotification(std::string m)	{
+	EnterCriticalSection(&critSec);
+	notifications.push(m);
+	LeaveCriticalSection(&critSec);	
+}
+
+void MessageQs::putMessage(std::string m)	{
+	EnterCriticalSection(&critSec);
+	messages.push(m);
+	LeaveCriticalSection(&critSec);	
+}
+
+void MessageQs::putAck(std::string m)	{
+	EnterCriticalSection(&critSec);
+	acks.push(m);
+	LeaveCriticalSection(&critSec);	
+}
+
+void MessageQs::putFileChunk(std::string m)	{
+	EnterCriticalSection(&critSec);
+	fileChunks.push(m);
+	LeaveCriticalSection(&critSec);	
+}
