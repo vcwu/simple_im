e203ba238a0344a8d68fe566cc30ef91f160dbef
Victoria Wu <victoria.chen.wu@gmail.com>
2013-03-03 22:43:26 -0600

    more stuff

--- MessageQs.h
+++ MessageQs.h
@@ -7,28 +7,55 @@
 
 #include <queue>
 #include <string>
-
+#include <iostream>
+#include <sstream>
 class MessageQs	{
 	private:
 		CRITICAL_SECTION critSec;	
+		CONDITION_VARIABLE ackStatus;
 		std::queue<std::string> notifications;
 		std::queue<std::string> messages;
 		std::queue<std::string> acks;
-		std::queue<std::string> fileChunks;
 	public:
 		MessageQs();
 		~MessageQs();
 		
+		void displayNotifications();	//prints out notifications.
+
+		void waitForAck(int msgNum);
 		void putNotification(std::string m);
 		void putMessage(std::string m);
 		void putAck(std::string m);
-		void putFileChunk(std::string m);
+
+		void getMessages();
 };
+
 
 MessageQs::MessageQs()	{
 	InitializeCriticalSection(&critSec);
 }
 MessageQs::~MessageQs()	{}
+
+void MessageQs::displayNotifications()	{
+	EnterCriticalSection(&critSec);
+	while(!notifications.empty())	{
+		std::cout << notifications.front() << std::endl;
+		notifications.pop();
+	}
+	std::cout << "NO NEW NOTIFICATIONS" << std::endl;
+	LeaveCriticalSection(&critSec);
+}
+
+void MessageQs::waitForAck(int msgNum)	{
+	EnterCriticalSection(&critSec);
+	while(acks.empty())	{
+		SleepConditionVariableCS(&ackStatus, &critSec, INFINITE);
+	}	
+
+	//acks queue now has something - check msg num
+	LeaveCriticalSection(&critSec);
+}
+
 
 void MessageQs::putNotification(std::string m)	{
 	EnterCriticalSection(&critSec);
@@ -48,8 +75,16 @@ void MessageQs::putAck(std::string m)	{
 	LeaveCriticalSection(&critSec);	
 }
 
-void MessageQs::putFileChunk(std::string m)	{
+void MessageQs::getMessages()	{
 	EnterCriticalSection(&critSec);
-	fileChunks.push(m);
+	if(messages.empty())	{
+		std::cout << "No new messages." << std::endl;
+	}
+	else	{
+		while(!messages.empty())	{
+			std::cout << messages.front() << std::endl;
+			messages.pop();
+		}
+	}
 	LeaveCriticalSection(&critSec);	
 }
