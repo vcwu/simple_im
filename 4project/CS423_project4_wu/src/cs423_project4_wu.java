212b7e76810f1edf78ca97466e9cb4811372a576
vptarmigan <vicky.ptarmigan@gmail.com>
2013-05-07 17:00:05 -0500

    Added cmd line interface

--- 4project/CS423_project4_wu/src/cs423_project4_wu.java
+++ 4project/CS423_project4_wu/src/cs423_project4_wu.java
@@ -4,6 +4,7 @@
  * @author victoria wu
  */
 
+import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class cs423_project4_wu {
@@ -16,6 +17,8 @@ public class cs423_project4_wu {
      */
     public static void main(String[] args) {
         
+        //Getting serverIP, port, and listenPort.
+        //--------------------------------------------
         String serverName = "134.193.128.197";
         int portNum = 3456;
         
@@ -37,8 +40,29 @@ public class cs423_project4_wu {
         String cmdLineArgs = String.format("Server IP: %s PortNum: %s ListenPort: %s", serverName, portNum, listenPort);
         LOGGER.info(cmdLineArgs);
         
+        //Starting up, logging on.
+        //--------------------------------------------        
         IM_Client cl = new IM_Client();
         cl.startup(serverName, portNum, listenPort, backLog);
+        
+        Scanner in = new Scanner(System.in);
+        System.out.println("What is your username?");
+        String userName = in.next();
+        cl.logOn(userName);
+        
+        String input ="";
+        Boolean userContinue = true;
+        while(userContinue) {
+            cl.displayMenu();
+            input = in.next();
+            switch(input.charAt(0)) {
+                case 's': cl.sendMessage(); break;
+                case 'f': cl.getFileNames(); break;
+                case 'd': cl.downloadFile(); break;
+                case 'q': userContinue = false; break;
+                default: break;
+            }
+        }
         
     }
 }
